4f4dd587e2964fffc68c1787cd72a0b398b52d71
Daniel <dan.m.bartel@gmail.com>
2015-04-08 14:47:13 -0500

    add trend api call

--- project/static/app/home/home.js
+++ project/static/app/home/home.js
@@ -2,7 +2,17 @@ define([
 	"angular",
 	"angularRoute"], function(angular) {
 		return angular.module("tbaApp.home", ["ui.router"])
-			.controller("HomeCtrl", ["$scope", function($scope) {
-				
-			}]);
-});
+			.controller("HomeCtrl", ["$scope", "$http", function($scope, $http) {
+                $scope.trends = [];
+
+
+                //fetch trends
+                $http({
+                    method: "GET",
+                    url: "/api/trends"
+                }).success(function(data,status,headers,config) {
+                    $scope.trends = data.trends;
+                });
+            }]);
+        });
+
