313384ad92f49476d0695d71ae45479163900d13
Daniel <dan.m.bartel@gmail.com>
2015-04-08 14:46:28 -0500

    added route stubs for api calls

--- project/app.py
+++ project/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import requests
+import keys
 
 app = Flask(__name__)
 
@@ -8,6 +10,23 @@ app.debug = True
 @app.route("/")
 def index():
 	return render_template("index.html")
+
+
+@app.route("/api/trends", methods=["GET"])
+def getTrends():
+    params = {"api_key": keys.TRENDING_KEY}
+    req = requests.get("http://api.whatthetrend.com/api/v2/trends.json", params=params)
+    return req.text
+
+@app.route("/api/tweets", methods=["GET", "POST"])
+def twitterOps():
+    pass
+
+@app.route("/api/words", methods=["GET"])
+def getWords():
+    pass
+
+
 
 if __name__ == "__main__":
 	app.run()
