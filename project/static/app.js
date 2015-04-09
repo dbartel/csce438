d71a9c2932b31bd2ea02cc0fc51c362e472a8ce3
Daniel <dan.m.bartel@gmail.com>
2015-04-08 19:02:21 -0500

    requirejs configuration for ngAnimate

--- project/static/app.js
+++ project/static/app.js
@@ -1,10 +1,12 @@
 define([
 	"angular", 
 	"angularRoute",
+    "angularAnimate",
 	"app/home/home"
 ], function(angular, angularRoute, home) {
 	return angular.module('tbaApp', [
 		"ui.router", 
+        "ngAnimate",
 		"tbaApp.home"
 	]);
 });
--- project/static/require-config.js
+++ project/static/require-config.js
@@ -2,11 +2,15 @@ require.config({
 	baseUrl: "/static/",
 	paths: {
 		angular: "/static/lib/angular/angular",
-		angularRoute: "/static/lib/angular/angular-ui-router.min"
+		angularRoute: "/static/lib/angular/angular-ui-router.min",
+		angularAnimate: "/static/lib/angular-animate/angular-animate"
 	},
 	shim: {
 		"angular" : {"exports" : "angular"},
 		"angularRoute" : {
+			deps: ["angular"]
+		},
+		"angularAnimate" : {
 			deps: ["angular"]
 		}
 	},
