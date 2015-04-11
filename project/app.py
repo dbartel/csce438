acfa9de3135463efd29be68e11664af9a2545541
Daniel <dan.m.bartel@gmail.com>
2015-04-11 14:19:52 -0500

    implemented twitter login

--- project/app.py
+++ project/app.py
@@ -1,15 +1,62 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, request
+from flask_oauth import OAuth
 import requests
 import keys
 
 app = Flask(__name__)
 
+
+# Twitter Authentication
+oauth = OAuth()
+
+twitter = oauth.remote_app("twitter",
+    base_url="https://api.twitter.com/1/",
+    request_token_url='https://api.twitter.com/oauth/request_token',
+    access_token_url='https://api.twitter.com/oauth/access_token',
+    authorize_url='https://api.twitter.com/oauth/authenticate',
+    consumer_key=keys.TWITTER_KEY,
+    consumer_secret=keys.TWITTER_SECRET
+)
+
+
+
 # Enable debug mode
 app.debug = True
+
+#Session secret key
+app.secret_key = keys.SESSION_SECRET
 
 @app.route("/")
 def index():
     return render_template("landingpage.html")
+
+
+@app.route("/login")
+def login():
+    return twitter.authorize(callback=url_for("twitterCallback", next=request.args.get('next') or request.referrer or None))
+
+@app.route("/logout")
+def logout():
+    session.pop("twitter_token", None)
+    return redirect(url_for("index"))
+
+@app.route("/twitter-login")
+@twitter.authorized_handler
+def twitterCallback(resp):
+    next_url = request.args.get("next") or url_for("tweet")
+    if session.has_key("twitter_token"): del session['twitter_token']
+# 
+    session["twitter_token"] = (
+        resp["oauth_token"],
+        resp["oauth_token_secret"]
+    )
+    session["twitter_user"] = resp["screen_name"]
+    return redirect(url_for("tweetBorrowing"))
+
+
+@twitter.tokengetter
+def get_twitter_token(token=None):
+    return session.get("twitter_token")
 
 
 @app.route("/tweet")
