1c494b05f1f4124aff478a4783ead0c9af46b101
Daniel <dan.m.bartel@gmail.com>
2015-04-12 15:02:35 -0500

    count word frequency in tweets

--- project/static/app/home/home.js
+++ project/static/app/home/home.js
@@ -1,6 +1,7 @@
 define([
 	"angular",
-	"angularRoute"], function(angular) {
+	"angularRoute",
+    "lodash"], function(angular,r,_) {
 		return angular.module("tbaApp.home", ["ui.router"])
 			.controller("HomeCtrl", ["$scope", "$http", "$timeout", function($scope, $http, $timeout) {
                 $scope.trends = [];
@@ -48,8 +49,48 @@ define([
                         params: { q:$scope.pickedTrend.name}
                     }).success(function(data, status, headers, config) {
                         $scope.tweets = data.statuses;
+                        $scope.getWordFrequency();
+
                     });
                 };
+
+                $scope.getWordFrequency = function() {
+                    var tweets = _.pluck($scope.tweets, "text");
+                    //reduce all tweets into one string
+                    var words = _.reduce(tweets, function(str, t) {
+                        return str + " " + t;
+                    });
+
+                    //tokenize string
+                    words = words.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+                    words = words.split(" ");
+
+                    //count frequencies
+                    var frequencies = {};
+                    _.forEach(words, function(w) {
+                        frequencies[w] = frequencies[w] || 0;
+                        frequencies[w]++;
+                    });
+
+                    $scope.wordFrequency = [];
+                    _.forIn(frequencies, function(value, key) {
+                        $scope.wordFrequency.push({
+                            word: key,
+                            freq: value
+                        });
+                    });
+
+                    //inverse sort
+                    $scope.wordFrequency = _.sortBy($scope.wordFrequency, function(n) {
+                        return -n.freq;
+                    });
+
+                    console.log($scope.wordFrequency);
+                    //slice down to top 15
+                    $scope.wordFrequency = _.slice($scope.wordFrequency, 0, 15);
+
+                }
+
 
             }]);
         });
