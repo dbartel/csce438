749351eef6164e2c21dd50c4ffc885993db4989c
Daniel <dan.m.bartel@gmail.com>
2015-04-19 12:56:54 -0500

    added keys.py

---
+++ project/keys.py
@@ -0,0 +1,7 @@
+from os import environ
+
+TRENDING_KEY = environ.get("TRENDING_KEY", None)
+TWITTER_KEY = environ.get("TWITTER_KEY", None)
+TWITTER_SECRET = environ.get("TWITTER_SECRET", None)
+MASHAPE_KEY = environ.get("MASHAPE_KEY", None)
+SESSION_SECRET = environ.get("SESSION_SECRET", None)
